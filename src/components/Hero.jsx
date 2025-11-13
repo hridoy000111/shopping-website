bd6259a64e55083395aeef24950821126f94ac3f
Md Redoy Hossen <redoykhan01943147885@gmai.com>
2025-11-14 00:31:13 +0800

    public folder image name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <img
         className="w-full rounded h-[40vh] md:h-[80vh] object-cover "
         src="/main.png"
-        alt="hero image"
+        alt="hero"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white mt-12 lg:mt-3">
         <h2 className="text-lg md:text-4xl font-bold    ">
