85fc7060a040556745dfdf9ba6b3474f99fab61a
Md Redoy Hossen <redoykhan01943147885@gmai.com>
2025-11-14 01:58:56 +0800

    SingleProductPage responsive

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -28,26 +28,30 @@ const ProductPage = () => {
   );
 
   return (
-    <div className=" container mx-auto pt-20 px-24 text-enter">
-      <div className="grid md:grid-cols-2 gap-6">
-        <div className="flex justify-start items-center my-2 py-3 ">
+    <div className=" container  overflow-x-hidden mx-auto pt-24  ">
+      <div className="grid md:grid-cols-2 gap-6  items-center">
+        {/* Image */}
+        <div className="flex justify-center ">
           <img
-            className="w-full h-auto max-w-xs object-contain hover:scale-105 transition-transform duration-300"
+            className="w-[250px] h-[250px] md:w-[350px] md:h-[350px] object-contain rounded-lg hover:scale-105 transition-transform duration-300"
             src={image}
             alt={title}
           />
         </div>
-        <div className="py-16 space-y-4 ">
+
+        {/* Product Details */}
+        <div className="space-y-6   ">
           <h4 className="uppercase text-xl text-gray-600 font-bold">
             {category}
           </h4>
-          <h1 className="text-3xl text-gray-500">{title}</h1>
-          <p className=" text-lg text-gray-500 my-2 flex items-center gap-2">
-            {rating.rate} <FaStar className="text-yellow-600"></FaStar>
+          <h1 className="text-2xl text-gray-500">{title}</h1>
+          <p className="text-lg text-gray-500 my-2 flex items-center gap-2">
+            {rating.rate} <FaStar className="text-yellow-600" />
           </p>
           <p className="text-3xl text-gray-500">${price}</p>
           <p className="text-gray-500">{description}</p>
-          <div className=" flex gap-4">
+
+          <div className="flex flex-wrap  gap-4">
             <button
               onClick={() => addToCart(product)}
               className="px-4 py-2 border border-black text-black rounded-lg hover:bg-black hover:text-white transition-all duration-300"
@@ -55,14 +59,15 @@ const ProductPage = () => {
               Add to Cart
             </button>
             <Link
-              className="px-4 py-2  bg-gray-900 text-white rounded-lg  hover:bg-gray-600 transition-all duration-300"
               to="/cart"
+              className="px-4 py-2 text-center bg-gray-900 text-white rounded-lg hover:bg-gray-600 transition-all duration-300"
             >
               Go To Cart
             </Link>
           </div>
         </div>
       </div>
+
       <div className=" hidden md:block  container mx-auto mt-12 ">
         <h2 className="text-2xl font-bold text-gray-700 mb-8">
           You may also like
